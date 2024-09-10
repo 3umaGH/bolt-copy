54576b60680daab3df6a5717174760077962aaaa
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-09-10 11:18:33 +0300

    fix: auto-scrolling

--- src/components/pages/MainPage/HorizontalSlider.tsx
+++ src/components/pages/MainPage/HorizontalSlider.tsx
@@ -16,37 +16,34 @@ export const HorizontalSlider = ({ data }: HorizontalSlider) => {
   const baseImages = [data[data.length - 1], ...data, ...data, data[0]]
   const slides = [...baseImages]
   const sliderRef = useRef<HTMLDivElement>(null)
-
-  console.log(slides)
+  const firstSlideRef = useRef<HTMLImageElement>(null) // Used to retreive slide width to scroll it correctly.
 
   const handleScrollNext = () => {
-    if (!sliderRef.current) {
+    if (!sliderRef.current || !firstSlideRef.current) {
       return
     }
 
     const slider = sliderRef.current
     const currentScroll = slider.scrollLeft
-    const containerWidth = slider.clientWidth
 
     slider.scrollTo({
-      left: currentScroll + containerWidth,
+      left: currentScroll + firstSlideRef.current.clientWidth,
       behavior: 'smooth',
     })
   }
 
-  // TODO: Fix, hacky solution
   useLayoutEffect(() => {
     setTimeout(() => {
       handleScrollNext()
     }, 200)
 
-    /* const scrollInterval = setInterval(() => {
-      handleScrollNext();
-    },5000)
+    const scrollInterval = setInterval(() => {
+      handleScrollNext()
+    }, 2000)
 
     return () => {
       clearInterval(scrollInterval)
-    }*/
+    }
   }, [sliderRef])
 
   const handleScroll = () => {
@@ -77,7 +74,12 @@ export const HorizontalSlider = ({ data }: HorizontalSlider) => {
       onScroll={handleScroll}
       className='flex w-screen gap-4 px-4 mt-2 overflow-auto snap-mandatory snap-x hide-scrollbar'>
       {slides.map((slide, index) => (
-        <img src={slide.img} key={`${slide.id}_${index}`} className={slideClassName} />
+        <img
+          ref={index === 0 ? firstSlideRef : null}
+          src={slide.img}
+          key={`${slide.id}_${index}`}
+          className={slideClassName}
+        />
       ))}
     </div>
   )
