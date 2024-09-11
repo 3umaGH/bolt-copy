6aadc3b3047c90a9ff0096c3247c2517d3dccab3
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-09-11 12:39:10 +0300

    style: interactive button

--- src/components/pages/MainPage/Restaurant/DishDetails/Footer.tsx
+++ src/components/pages/MainPage/Restaurant/DishDetails/Footer.tsx
@@ -12,7 +12,7 @@ export const Footer = memo(({ price }: Footer) => {
       <hr />
       <div className='flex items-center justify-between gap-4 p-2'>
         <Stepper min={1} max={10} onChange={setAmount} />
-        <button className='flex-1 px-4 py-1 font-bold text-white bg-green-500 rounded-full max-w-[500px]'>
+        <button className='flex-1 px-4 py-1 font-bold text-white bg-green-500 hover:bg-green-400 transition-all active:scale-[0.98] rounded-full max-w-[500px]'>
           <div className='flex flex-col items-center justify-center'>
             <span className='text-sm'>Add</span>
             <span className='text-xs'>{(price * amount).toFixed(2)} €</span>
