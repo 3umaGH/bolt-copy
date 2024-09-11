4bed44976f4aed6e7040a0aa8de85781b812d6be
3umaGH <133604702+3umaGH@users.noreply.github.com>
2024-09-11 18:49:36 +0300

    feat: disclaimer, scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import { Restaurant } from './pages/Restaurant'
+import { Disclaimer } from './components/Disclaimer'
+import ScrollToTop from './components/common/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <Disclaimer />
+      <ScrollToTop />
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/restaurant/:id' element={<Restaurant />} />
---
+++ src/components/Disclaimer.tsx
@@ -0,0 +1,13 @@
+export const Disclaimer = () => {
+  return (
+    <div className='fixed top-0 z-[1000] bg-white hidden w-screen h-screen p-2 lg:block text-center'>
+      <p className='text-xl font-bold'>Disclaimer</p>
+      <p>
+        This demo showcases my web development skills by replicating the design of the 'Bolt Food' mobile app. <br />{' '}
+        Please access the site on a mobile device, as larger viewports are not supported.
+      </p>
+
+      <p className='mt-4'>Note: This demo or me is not affiliated with Bolt company in any way.</p>
+    </div>
+  )
+}
